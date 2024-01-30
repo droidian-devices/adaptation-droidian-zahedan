10ea5fcc084ee232225547a7be4aa9f7e3389215
Bardia Moshiri <fakeshell@bardia.tech>
2024-01-30 02:31:02 -0500

    pbhelper: use a non blocking sys call to take care of volume buttons and other actions turning on the screen while keeping input detection intact

--- src/pbhelper.c
+++ src/pbhelper.c
@@ -11,8 +11,9 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdarg.h>
-#include <batman/wlrdisplay.h>
+#include <sys/select.h>
 #include <linux/input.h>
+#include <batman/wlrdisplay.h>
 
 // Convert seconds to micros
 #define WRITE_SLEEP_PERIOD 0.1*1000000
@@ -25,12 +26,12 @@
 #define DEVICE_FILE "/dev/input/event1"
 
 void debug(const char* format, ...) {
-    #ifdef DEBUG
-        va_list args;
-        va_start(args, format);
-        vprintf(format, args);
-        va_end(args);
-    #endif
+#ifdef DEBUG
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+#endif
 }
 
 int main(int argc, char* argv[]) {
@@ -42,6 +43,8 @@ int main(int argc, char* argv[]) {
     int bread_before;
     bool first_run = true;
     bool screen_on = true;
+    bool screen_interrupted = false;
+    bool screen_interrupted_old = true;
 
     struct input_event ev;
     int fd_device = open(DEVICE_FILE, O_RDONLY);
@@ -50,44 +53,85 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Set fd_device to non-blocking mode
+    int flags = fcntl(fd_device, F_GETFL, 0);
+    fcntl(fd_device, F_SETFL, flags | O_NONBLOCK);
+
+    fd_set readfds;
+    struct timeval tv;
+    int ret;
+
     while (1) {
-        read(fd_device, &ev, sizeof(struct input_event));
-        if (ev.type == EV_KEY && ev.value == 1 && ev.code == 116) { // Power button pressed
-            screen_on = !screen_on;
-            debug("Screen state changed: %s\n", screen_on ? "On" : "Off");
+        FD_ZERO(&readfds);
+        FD_SET(fd_device, &readfds);
 
-            if (screen_on) {
-                // Screen turned on
-                if (!first_run) {
-                    debug("Setting Brightness to 0\n");
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
 
-                    // Setting to zero since the values must differ to turn on the screen
-                    brightness_control = open("/sys/class/leds/lcd-backlight/brightness",O_WRONLY);
-                    bwritten = write(brightness_control, "0", 1);
-                    close(brightness_control);
-                    debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
+        ret = select(fd_device + 1, &readfds, NULL, NULL, &tv);
 
-                    usleep(WRITE_SLEEP_PERIOD); // without this it will get confused for whatever reason
+        if (ret > 0 && FD_ISSET(fd_device, &readfds)) {
+            if (read(fd_device, &ev, sizeof(struct input_event)) > 0) {
+                if (ev.type == EV_KEY && ev.value == 1 && ev.code == 116) { // Power button pressed
+                    screen_on = wlrdisplay(argc, argv);
+                    debug("Screen state changed: %s\n", screen_on ? "On" : "Off");
 
-                    debug("Setting Brightness to %s\n", before_lock);
-                    brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_WRONLY);
-                    bwritten = write(brightness_control, before_lock, strlen(before_lock));
-                    close(brightness_control);
+                    if (screen_on) {
+                        if (!first_run) {
+                            debug("Setting Brightness to 0\n");
 
-                    debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
+                            brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_WRONLY);
+                            bwritten = write(brightness_control, "0", 1);
+                            close(brightness_control);
+                            debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
+
+                            usleep(WRITE_SLEEP_PERIOD);
+
+                            debug("Setting Brightness to %s\n", before_lock);
+                            brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_WRONLY);
+                            bwritten = write(brightness_control, before_lock, strlen(before_lock));
+                            close(brightness_control);
+
+                            debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
+                        }
+                    } else {
+                        memset(before_lock, 0, sizeof(before_lock));
+                        brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_RDONLY);
+                        bread_before = read(brightness_control, before_lock, sizeof(before_lock) - 1);
+                        close(brightness_control);
+
+                        debug(READ_FORMAT, bread_before, "/sys/class/leds/lcd-backlight/brightness");
+                        debug("Current Brightness: %s\n", before_lock);
+
+                        first_run = false;
+                    }
                 }
-            } else {
-                // Screen turned off, save current brightness
-                memset(before_lock, 0, sizeof(before_lock));
-                brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_RDONLY);
-                bread_before = read(brightness_control, before_lock, sizeof(before_lock) - 1);
+            }
+        }
+
+        screen_interrupted = !wlrdisplay(argc, argv);
+        debug("screen_interrupted: %d screen_interrupted_old: %d, screen_on: %d\n", screen_interrupted, screen_interrupted_old, screen_on);
+        if (screen_interrupted != screen_interrupted_old) {
+            debug("Screen state changed: %s\n", screen_interrupted ? "On" : "Off");
+
+            if (screen_interrupted && screen_on == 0) {
+                debug("Setting Brightness to 0\n");
+
+                brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_WRONLY);
+                bwritten = write(brightness_control, "0", 1);
+                close(brightness_control);
+                debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
+
+                usleep(WRITE_SLEEP_PERIOD);
+
+                debug("Setting Brightness to %s\n", before_lock);
+                brightness_control = open("/sys/class/leds/lcd-backlight/brightness", O_WRONLY);
+                bwritten = write(brightness_control, before_lock, strlen(before_lock));
                 close(brightness_control);
 
-                debug(READ_FORMAT, bread_before, "/sys/class/leds/lcd-backlight/brightness");
-                debug("Current Brightness: %s\n", before_lock);
-
-                first_run = false;
+                debug(WRITE_FORMAT, bwritten, "/sys/class/leds/lcd-backlight/brightness");
             }
+            screen_interrupted_old = screen_interrupted;
         }
     }
 
